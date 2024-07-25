1e6bb7daa88b13fe6a1903fe78b4323d11f063bd
param <spsummerdoc@gmail.com>
2024-07-25 16:44:06 +0900

    feat: 새로운기능 추가 테스트

--- src/main/java/hello/techit7_branch/dev/dev.java
+++ src/main/java/hello/techit7_branch/dev/dev.java
@@ -3,6 +3,7 @@ package hello.techit7_branch.dev;
 public class dev {
     public static void main(String[] args) {
         System.out.println("하이하이");
+        System.out.println("ㄴㅀㄶ");
 
     }
 }
